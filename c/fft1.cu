cb6f48263e532af2a5bf9f1b4f33bd00baa276ae
HarbinZhang <HarbinZhang@users.noreply.github.com>
2017-12-03 16:28:03 -0500

    Update fft1.cu

--- c/fft1.cu
+++ c/fft1.cu
@@ -1,13 +1,13 @@
 #include <cufft.h>
 #define NX 256
 #define BATCH 10
-cufftHandle  plan; 
-cufftComplex  *data; 
-cudaMalloc((void**)&data, sizeof(cufftComplex)*NX*BATCH);
-/* Create a 1D FFT plan. */ 
-cufftPlan1d(&plan, NX, CUFFT  C2C, BATCH); 
-/* Use the CUFFT plan to transform the signal in place. */ 
-cufftExecC2C(plan, data, data, CUFFT  FORWARD); 
-/* Destroy the CUFFT plan. */ 
-cufftDestroy(plan); 
-cudaFree(data); 
+cufftHandle plan;
+cufftComplex *data;
+cudaMalloc((void**)&data,sizeof(cufftComplex)*NX*BATCH);
+/* Create a 1D FFT plan. */
+cufftPlan1d(&plan,NX,CUFFT C2C,BATCH);
+/* Use the CUFFT plan to transform the signal in place. */
+cufftExecC2C(plan,data,data,CUFFT FORWARD);
+/* Destroy the CUFFT plan. */
+cufftDestroy(plan);
+cudaFree(data);
