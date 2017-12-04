cf25f82fc908f6b78cceed010c4d0b5731a68de7
HarbinZhang <seashore.real@gmail.com>
2017-12-03 21:37:43 -0500

    auto

---
+++ test/view.py
@@ -0,0 +1,15 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+
+infile = "data"
+
+infile = open(infile, 'r')
+
+data = infile.read().split('\n')
+
+
+
+plt.plot(data)
+plt.show()
